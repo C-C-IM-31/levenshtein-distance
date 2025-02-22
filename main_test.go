b40d94610296ff851bcf1f0c60c2daf1d81886ac
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-02-22 21:46:26 +0200

    Initial commit

---
+++ main_test.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLevenshteinDistance(t *testing.T) {
+	for _, tc := range []struct {
+		name   string
+		a, b   string
+		wanted int
+	}{
+		{name: "empty", a: "", b: "", wanted: 0},
+		{name: "equal", a: "abc", b: "abc", wanted: 0},
+		{name: "insertions", a: "ab", b: "abc", wanted: 1},
+		{name: "insertions-2", a: "ab", b: "dabc", wanted: 2},
+		{name: "deletions", a: "abc", b: "ab", wanted: 1},
+		{name: "editions", a: "abc", b: "adc", wanted: 1},
+		{name: "sentence", a: "cat and dog", b: "dog and cat", wanted: 6},
+	} {
+		// START OMIT
+		t.Run(tc.name, func(t *testing.T) {
+			if got := LevenshteinDistance(tc.a, tc.b); got != tc.wanted {
+				t.Errorf("wanted %d, got %d", tc.wanted, got)
+			}
+			if got := LevenshteinDistance(tc.b, tc.a); got != tc.wanted {
+				t.Errorf("inverted - wanted %d, got %d", tc.wanted, got)
+			}
+		})
+		// END OMIT
+	}
+}
+
+func BenchmarkLevenshteinDistance(b *testing.B) {
+	b.Run("short", func(b *testing.B) {
+		for b.Loop() {
+			if got := LevenshteinDistance("abc", "def"); got == 0 {
+				b.Error("non-zero distance")
+			}
+		}
+	})
+	b.Run("medium", func(b *testing.B) {
+		for b.Loop() {
+			if got := LevenshteinDistance("abcedf", "defabc"); got == 0 {
+				b.Error("non-zero distance")
+			}
+		}
+	})
+	b.Run("longer", func(b *testing.B) {
+		for b.Loop() {
+			if got := LevenshteinDistance("abcedfabcedf", "vbcedfabcedf"); got == 0 {
+				b.Error("non-zero distance")
+			}
+		}
+	})
+}
+
+func ExampleLevenshteinDistance() {
+	fmt.Println(LevenshteinDistance("abc", "adc"))
+}
+
+func main() {
+	ExampleLevenshteinDistance()
+}
