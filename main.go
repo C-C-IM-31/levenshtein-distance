2a62457fb6795ff81493d5f1ac4f5700f8ac3d4c
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-02-22 22:21:12 +0200

    Add full matrix algorithm

--- main.go
+++ main.go
@@ -1,19 +1,53 @@
 package main
 
+//func LevenshteinDistance(a, b string) int {
+//	if len(a) == 0 {
+//		return len(b)
+//	}
+//	if len(b) == 0 {
+//		return len(a)
+//	}
+//
+//	x := LevenshteinDistance(a, b[1:]) + 1
+//	y := LevenshteinDistance(a[1:], b) + 1
+//	z := LevenshteinDistance(a[1:], b[1:])
+//	if a[0] != b[0] {
+//		z++
+//	}
+//
+//	return min(x, y, z)
+//}
+
 func LevenshteinDistance(a, b string) int {
-	if len(a) == 0 {
-		return len(b)
-	}
-	if len(b) == 0 {
-		return len(a)
+	aLen, bLen := len(a), len(b)
+
+	dp := make([][]int, aLen+1)
+	for i := range dp {
+		dp[i] = make([]int, bLen+1)
 	}
 
-	x := LevenshteinDistance(a, b[1:]) + 1
-	y := LevenshteinDistance(a[1:], b) + 1
-	z := LevenshteinDistance(a[1:], b[1:])
-	if a[0] != b[0] {
-		z++
+	for i := 0; i <= aLen; i++ {
+		dp[i][0] = i
+	}
+	for j := 0; j <= bLen; j++ {
+		dp[0][j] = j
 	}
 
-	return min(x, y, z)
+	for i := 1; i <= aLen; i++ {
+		for j := 1; j <= bLen; j++ {
+			dpTempPrevious := dp[i-1]
+			dpTemp := dp[i]
+			if a[i-1] == b[j-1] {
+				dpTemp[j] = dpTempPrevious[j-1]
+			} else {
+				dpTemp[j] = 1 + min(
+					dpTemp[j-1],
+					dpTempPrevious[j],
+					dpTempPrevious[j-1],
+				)
+			}
+		}
+	}
+
+	return dp[aLen][bLen]
 }
