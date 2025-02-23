10177f8df943b748cda10326c6ec7cae56239add
Lytvynenko Serhii <serrheylitvinenko@gmail.com>
2025-02-23 02:55:11 +0200

    remove comments

--- main.go
+++ main.go
@@ -1,23 +1,5 @@
 package main
 
-//func LevenshteinDistance(a, b string) int {
-//	if len(a) == 0 {
-//		return len(b)
-//	}
-//	if len(b) == 0 {
-//		return len(a)
-//	}
-//
-//	x := LevenshteinDistance(a, b[1:]) + 1
-//	y := LevenshteinDistance(a[1:], b) + 1
-//	z := LevenshteinDistance(a[1:], b[1:])
-//	if a[0] != b[0] {
-//		z++
-//	}
-//
-//	return min(x, y, z)
-//}
-
 func LevenshteinDistance(a, b string) int {
 	aLen, bLen := len(a), len(b)
 
