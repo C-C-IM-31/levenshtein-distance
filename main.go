16e05cdd435c0d8065ebafbbccb3333657c2150e
Lytvynenko Serhii <serrheylitvinenko@gmail.com>
2025-02-23 03:14:10 +0200

    add algorithm

--- main.go
+++ main.go
@@ -1,19 +1,36 @@
 package main
 
-func LevenshteinDistance(a, b string) int {
+var cache = make(map[string]int)
+
+func generateKey(a, b string) string {
+	return a + "|" + b
+}
+
+func calculate(a, b string) int {
 	if len(a) == 0 {
 		return len(b)
 	}
 	if len(b) == 0 {
 		return len(a)
 	}
-
-	x := LevenshteinDistance(a, b[1:]) + 1
-	y := LevenshteinDistance(a[1:], b) + 1
-	z := LevenshteinDistance(a[1:], b[1:])
-	if a[0] != b[0] {
-		z++
+	if (a[0] == b[0]) {
+		return LevenshteinDistance(a[1:], b[1:])
 	}
+	atail := a[1:]
+	btail := b[1:]
+	first := LevenshteinDistance(atail, b)
+	second := LevenshteinDistance(a, btail)
+	third := LevenshteinDistance(atail, btail)
+	return min(first, second, third) + 1
+}
 
-	return min(x, y, z)
+func LevenshteinDistance(a, b string) int {
+	hash := generateKey(a, b)
+	value, exists := cache[hash]
+	if exists {
+		return value
+	}
+	result := calculate(a, b)
+	cache[hash] = result
+	return result
 }
