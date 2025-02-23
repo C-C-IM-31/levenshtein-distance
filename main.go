a869934cba80a27c648bc464727b5275de164810
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-02-23 11:24:23 +0200

    Add file for running tests conveniently

--- main.go
+++ main.go
@@ -1,33 +1,20 @@
 package main
 
-func get(matrix [][]int, row, col int) int {
-	if row == 0 && col == 0 {
-		return 0
-	}
-	if row == 0 && col > 0 {
-		return col
-	}
-	if col == 0 && row > 0 {
-		return row
-	}
-	return matrix[row - 1][col - 1]
-}
-
-func LevenshteinDistance(a, b string) int {
-	rows, cols := len(a), len(b)
-	matrix := make([][]int, rows)
-	for i := 1; i < rows + 1; i++ {
-		row := make([]int, cols)
-		matrix[i - 1] = row
-		for j := 1; j < cols + 1; j++ {
-			first := get(matrix, i, j - 1) + 1
-			second := get(matrix, i - 1, j) + 1
-			third := get(matrix, i - 1, j - 1)
-			if a[i - 1] != b[j - 1] {
-				third += 1
-			}
-			row[j - 1] = min(first, second, third)
-		}
-	}
-	return get(matrix, rows, cols)
-}
+//
+//func LevenshteinDistance(a, b string) int {
+//	if len(a) == 0 {
+//		return len(b)
+//	}
+//	if len(b) == 0 {
+//		return len(a)
+//	}
+//
+//	x := LevenshteinDistance(a, b[1:]) + 1
+//	y := LevenshteinDistance(a[1:], b) + 1
+//	z := LevenshteinDistance(a[1:], b[1:])
+//	if a[0] != b[0] {
+//		z++
+//	}
+//
+//	return min(x, y, z)
+//}
