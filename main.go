d70a5115647066e715fd27614290ab59364d0665
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-02-23 11:18:22 +0200

    Refactor code

--- main.go
+++ main.go
@@ -2,23 +2,23 @@ package main
 
 func LevenshteinDistance(a, b string) int {
 	rows, cols := len(a), len(b)
-	previosRow := make([]int, cols + 1)
-	currentRow := make([]int, cols + 1)
+	previousRow := make([]int, cols+1)
+	currentRow := make([]int, cols+1)
 	for index := range cols {
-		previosRow[index] = index
+		previousRow[index] = index
 	}
-	for i := 1; i < rows + 1; i++ {
+	for i := 1; i < rows+1; i++ {
 		currentRow[0] = i
-		for j := 1; j < cols + 1; j++ {
-			first := currentRow[j - 1] + 1
-			second := previosRow[j] + 1
-			third := previosRow[j - 1]
+		for j := 1; j < cols+1; j++ {
+			first := currentRow[j-1] + 1
+			second := previousRow[j] + 1
+			third := previousRow[j-1]
 			if a[i-1] != b[j-1] {
 				third += 1
 			}
 			currentRow[j] = min(first, second, third)
 		}
-		copy(previosRow, currentRow)
+		copy(previousRow, currentRow)
 	}
 	return currentRow[cols]
 }
