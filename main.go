2dd3bad06117602be828ba187e13dd8e5d821657
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-02-24 22:06:57 +0200

    Add file for running tests conveniently

--- main.go
+++ main.go
@@ -1,36 +1,20 @@
 package main
 
-var cache = make(map[string]int)
-
-func generateKey(a, b string) string {
-	return a + "|" + b
-}
-
-func calculate(a, b string) int {
-	if len(a) == 0 {
-		return len(b)
-	}
-	if len(b) == 0 {
-		return len(a)
-	}
-	if a[0] == b[0] {
-		return LevenshteinDistance(a[1:], b[1:])
-	}
-	aTail := a[1:]
-	bTail := b[1:]
-	first := LevenshteinDistance(aTail, b)
-	second := LevenshteinDistance(a, bTail)
-	third := LevenshteinDistance(aTail, bTail)
-	return min(first, second, third) + 1
-}
-
-func LevenshteinDistance(a, b string) int {
-	hash := generateKey(a, b)
-	value, exists := cache[hash]
-	if exists {
-		return value
-	}
-	result := calculate(a, b)
-	cache[hash] = result
-	return result
-}
+//
+//func LevenshteinDistance(a, b string) int {
+//	if len(a) == 0 {
+//		return len(b)
+//	}
+//	if len(b) == 0 {
+//		return len(a)
+//	}
+//
+//	x := LevenshteinDistance(a, b[1:]) + 1
+//	y := LevenshteinDistance(a[1:], b) + 1
+//	z := LevenshteinDistance(a[1:], b[1:])
+//	if a[0] != b[0] {
+//		z++
+//	}
+//
+//	return min(x, y, z)
+//}
